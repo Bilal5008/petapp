28a7755c17d0f0ad9cdbf9566d092e993ff0671d
Bilal <muhammad.bilal@digitify.com>
2021-10-04 15:37:27 +0500

    navigation with args

--- lib/Common/routes.dart
+++ lib/Common/routes.dart
@@ -1,7 +1,31 @@
-class Routes{
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:petapp/feature/moviedetail/moviedetailscreen.dart';
+import 'package:petapp/feature/movielist/movielistscreen.dart';
 
-  static  String loginRoute = 'Login';
-  static const String forgetPasswordRoute = '/Authentication/Login/forgetpassword';
+class Routes {
+  static String loginRoute = 'Login';
+  static const String forgetPasswordRoute =
+      '/Authentication/Login/forgetpassword';
   static const String signUpRoute = 'SignUp';
   static const String dashboardRoute = 'Dashboard';
+  static const routeName = '/passArguments';
+  static const routeName2 = '/passArguments2';
 }
+
+// variable for our route names
+const String movieList = 'movieList';
+const String movieDetails = 'movieDetails';
+
+// controller function with switch statement to control page route flow
+// Route<dynamic> controller(RouteSettings settings) {
+//   switch (settings.name) {
+//     case movieList:
+//       return MaterialPageRoute(builder: (context) => MovieListScreen());
+//     case movieDetails:
+//       return MaterialPageRoute(builder: (context) => MovieDetailScreen());
+//     default:
+//       throw ('this route name does not exist');
+//   }
+// }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:petapp/feature/moviedetail/moviedetailscreen.dart';
+import 'package:petapp/feature/movielist/movielistresponse/alllist_response.dart';
 import 'package:petapp/networking/app_config.dart';
 import 'package:petapp/networking/environment.dart';
-import 'Authentication/DashBoard/addpet.dart';
-import 'Authentication/Login/forgetpassword.dart';
-import 'Authentication/SignUp/signup.dart';
+
 import 'Common/routes.dart';
+import 'feature/moviedetail/moviedetailscreen2.dart';
 import 'feature/movielist/movielistscreen.dart';
 
 void main() {
@@ -16,16 +17,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     AppConfig.instance.setEnvironment(Environment.dev);
     return MaterialApp(
-      routes: {
-        Routes.forgetPasswordRoute: (context) => ForgetPasswordScreen(),
-        Routes.signUpRoute: (context) => SignUpScreen(),
-        Routes.dashboardRoute: (context) => AddPetScreen(),
-      },
       title: 'Splash Screen',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.indigo,
       ),
-      home: MovieListScreen(),
+
+      // routes: {
+      //   '/details': (context) => MovieDetailScreen(),
+      // },
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        // If you push the PassArguments route
+        if (settings.name == Routes.routeName) {
+          // Cast the arguments to the correct
+          // type: ScreenArguments.
+          final args = settings.arguments as Result;
+
+          // Then, extract the required data from
+          // the arguments and pass the data to the
+          // correct screen.
+          return MaterialPageRoute(
+            builder: (context) {
+              return MovieDetailScreen(
+                result: args,
+              );
+            },
+          );
+        } else if (settings.name == Routes.routeName2) {
+          final args = settings.arguments as Result;
+
+          // Then, extract the required data from
+          // the arguments and pass the data to the
+          // correct screen.
+          return MaterialPageRoute(
+            builder: (context) {
+              return MovieDetailScreen2(
+                result: args,
+              );
+            },
+          );
+        } else if (settings.name == "/") {
+          return MaterialPageRoute(
+            builder: (context) {
+              return MovieListScreen();
+            },
+          );
+        }
+        // The code only supports
+        // PassArgumentsScreen.routeName right now.
+        // Other values need to be implemented if we
+        // add them. The assertion here will help remind
+        // us of that higher up in the call stack, since
+        // this assertion would otherwise fire somewhere
+        // in the framework.
+        assert(false, 'Need to implement ${settings.name}');
+        return null;
+      },
+
       debugShowCheckedModeBanner: false,
     );
   }
